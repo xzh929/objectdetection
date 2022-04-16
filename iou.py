8c5a705c3b829a68e79feeffaecb679fda3b76b7
fallen <921352686@qq.com>
2022-04-16 17:04:26 +0800

    little update

--- iou.py
+++ iou.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-def IOU(box, boxes,isMin=False):
+
+def IOU(box, boxes, isMin=False):
     box_area = (box[:, 2] - box[:, 0]) * (box[:, 3] - box[:, 1])
     boxes_area = (boxes[:, 2] - boxes[:, 0]) * (boxes[:, 3] - boxes[:, 1])
 
@@ -9,19 +10,19 @@ def IOU(box, boxes,isMin=False):
     rx = np.minimum(box[:, 2], boxes[:, 2])
     ry = np.minimum(box[:, 3], boxes[:, 3])
 
-    w = np.maximum(0,rx-lx)
-    h = np.maximum(0,ry-ly)
+    w = np.maximum(0, rx - lx)
+    h = np.maximum(0, ry - ly)
 
-    inter = w*h
+    inter = w * h
     if isMin:
-        ovr = np.true_divide((inter,np.minimum(box_area,boxes_area)))
+        ovr = np.true_divide((inter, np.minimum(box_area, boxes_area)))
     else:
-        ovr = np.true_divide(inter,(box_area+boxes_area-inter))
+        ovr = np.true_divide(inter, (box_area + boxes_area - inter))
 
     return ovr
 
 
 if __name__ == '__main__':
-    a = np.array([[1,1,10,10],[10,10,20,20]])
-    b = np.array([[2,2,8,8],[40,40,50,50]])
-    print(IOU(a,b))
+    a = np.array([[1, 1, 10, 10], [10, 10, 20, 20]])
+    b = np.array([[2, 2, 8, 8], [40, 40, 50, 50]])
+    print(IOU(a, b))
--- nms.py
+++ nms.py
@@ -1,17 +1,19 @@
 import numpy as np
 from iou import IOU
-def nms(boxes,thresh=0.3,isMin=False):
+
+
+def nms(boxes, thresh=0.3, isMin=False):
     if boxes.shape[0] == 0:
         return np.array([])
-    _boxes = boxes[(-boxes[:,4]).argsort()]
+    _boxes = boxes[(-boxes[:, 4]).argsort()]
 
     r_boxes = []
-    while _boxes.shape[0]>1:
+    while _boxes.shape[0] > 1:
         a_box = _boxes[0]
         b_boxes = _boxes[1:]
         r_boxes.append(a_box)
-        index = np.where(iou(a_box,b_boxes,isMin))
+        index = np.where(IOU(a_box, b_boxes, isMin) < thresh)
         _boxes = b_boxes[index]
-    if _boxes.shape[0]>0:
+    if _boxes.shape[0] > 0:
         r_boxes.append(_boxes[0])
     return np.stack(r_boxes)
